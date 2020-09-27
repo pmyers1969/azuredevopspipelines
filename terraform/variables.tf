ea421d9a0d8cc3b3924b1b3189d5d436fe67ce29
Paul Myers <pmyers1969@hotmail.com>
2020-09-27 14:38:19 +1000

    remove files

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  default = "development"
-  description = "The Environment name, i.e dev, staging, qa, preprod, test, prod..."
-}
-
-variable "resource-group-name" {
-  default = "resourcegroup"
-  description = "The Resource Group name to be deployed"
-}
